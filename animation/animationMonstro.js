b3f28f1ebf44272e65ca62d4ec60ad25ac0b32a5
reidnersousa <sousareidner72@gmail.com>
2023-06-15 21:54:57 +0000

    animationMonstroAdjust

--- animation/animationMonstro.js
+++ animation/animationMonstro.js
@@ -1,26 +1,21 @@
 //animation.js
 export class AnimationMonstro {
   static createAnimations(scene) {
-    scene.anims.create({
-      key: 'parado',
-      frames: scene.anims.generateFrameNumbers('personagem', { start: 1, end: 3 }),
-      frameRate: 2,
-      repeat: -1
-    });
 
     scene.anims.create({
-      key: 'direita',
-      frames: scene.anims.generateFrameNumbers('personagem', { start: 11, end: 19 }),
-      frameRate: 10,
-      repeat: -1
-    });
+    key : 'vox_esquerda',
+    flipX:true,
+    frames : this.anims.generateFrameNumbers('vox', { start : 1, end : 11}),
+    frameRate:10,
+    repeat : -1
+  });
 
-    scene.anims.create({
-      key: 'esquerda',
-      frames: scene.anims.generateFrameNumbers('personagem', { start: 11, end: 19 }),
-      frameRate: 10,
-      repeat: -1,
-      flipX: true
-    });
+  scene.anims.create({
+    key : 'vox_direita',
+    frames : this.anims.generateFrameNumbers('vox', { start : 1,end:13}),
+    frameRate: 10,
+    repeat : -1
+  });
+    
   }
 }
